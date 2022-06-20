92f1e214e5c1215a8f8524ac278a84c9f2d625d0
Sun <sonya8412@gmail.com>
2022-06-20 12:05:18 +0900

    Recipe Join
    GET /recipebox/join/recipe

---
+++ src/main/java/kr/momjobgo/eyou/web/jpa/repository/RecipeBoxRepository.java
@@ -0,0 +1,7 @@
+package kr.momjobgo.eyou.web.jpa.repository;
+
+import kr.momjobgo.eyou.web.jpa.entity.RecipeBoxEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecipeBoxRepository extends JpaRepository<RecipeBoxEntity, Long> {
+}
