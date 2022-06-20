9aef75cb86c0a62eac7a7a34c187f41275b92903
Sun <sonya8412@gmail.com>
2022-06-20 15:49:52 +0900

    RecipeBoxView initial work
    RecipeBoxView initial work

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/write">{{ $t("title.writeRecipe")}}</router-link> |
+      <router-link to="/recipebox">{{ $t("title.recipeBox")}}</router-link> |
       <router-link to="/login">login</router-link>  |
       <router-link to="/about">About</router-link>
     </nav>
