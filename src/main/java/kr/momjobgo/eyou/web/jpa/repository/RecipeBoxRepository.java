cda0a82a0690630b28728c7700b517885dc846f9
Sun <sonya8412@gmail.com>
2022-06-21 14:12:06 +0900

    @Repository Annotation

--- src/main/java/kr/momjobgo/eyou/web/jpa/repository/RecipeBoxRepository.java
+++ src/main/java/kr/momjobgo/eyou/web/jpa/repository/RecipeBoxRepository.java
@@ -2,6 +2,8 @@ package kr.momjobgo.eyou.web.jpa.repository;
 
 import kr.momjobgo.eyou.web.jpa.entity.RecipeBoxEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeBoxRepository extends JpaRepository<RecipeBoxEntity, Long> {
 }
--- src/main/java/kr/momjobgo/eyou/web/jpa/repository/RecipeRepository.java
+++ src/main/java/kr/momjobgo/eyou/web/jpa/repository/RecipeRepository.java
@@ -2,7 +2,9 @@ package kr.momjobgo.eyou.web.jpa.repository;
 
 import kr.momjobgo.eyou.web.jpa.entity.RecipeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeRepository extends JpaRepository<RecipeEntity, Long> {
 
 }
