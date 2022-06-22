f07f4e0196ad045820f557f871ad1c07f4410c59
Sun <sonya8412@gmail.com>
2022-06-22 23:10:11 +0900

    ManyToMany JoinTable Test

--- src/test/java/kr/momjobgo/eyou/EyouApplicationTests.java
+++ src/test/java/kr/momjobgo/eyou/EyouApplicationTests.java
@@ -1,12 +1,14 @@
 package kr.momjobgo.eyou;
 
 import kr.momjobgo.eyou.web.controller.RecipeBoxController;
-import kr.momjobgo.eyou.web.jpa.entity.RecipeBoxEntity;
-import kr.momjobgo.eyou.web.jpa.entity.RecipeEntity;
-import kr.momjobgo.eyou.web.jpa.entity.UserEntity;
+import kr.momjobgo.eyou.web.jpa.entity.*;
+import kr.momjobgo.eyou.web.jpa.repository.MemberRepository;
 import kr.momjobgo.eyou.web.jpa.repository.RecipeBoxRepository;
 import kr.momjobgo.eyou.web.jpa.repository.RecipeRepository;
+import kr.momjobgo.eyou.web.jpa.repository.TeacherRepository;
+import kr.momjobgo.eyou.web.service.MemberService;
 import kr.momjobgo.eyou.web.service.RecipeBoxService;
+import kr.momjobgo.eyou.web.service.TeacherService;
 import org.hibernate.validator.internal.IgnoreForbiddenApisErrors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +16,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
@@ -32,46 +35,85 @@ class EyouApplicationTests {
     @Autowired
     RecipeBoxController recipeBoxController;
 
+    @Autowired
+    TeacherRepository teacherRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    TeacherService teacherService;
+
+
     @BeforeEach
     void init() {
     }
 
-//    @Disabled
-//    @Test
-//    void contextLoads() {
-//        UserEntity user = new UserEntity();
-//        RecipeBoxEntity recipeBox = new RecipeBoxEntity();
-//        RecipeEntity recipe = new RecipeEntity();
-//
-//        // CREATE
-//        // UPDATE
-////        id 에 해당하는 name 변경은 어떻게 하지?
-////        recipeBox.setId(1L);
-//        recipeBox.setName("호박레시피");
-//        recipeBox.setIsDefault(false);
-//        recipeBoxRepository.save(recipeBox);
-//
-//        recipe.setPeriod("중기");
-//        recipe.setOpen("Y");
-//        recipe.setQuantity("1회분");
-//        recipeBox.add(recipe);
-//        recipeRepository.save(recipe);
-//
-//        // GET
-//        recipeRepository.findAll();
-//        System.out.println(recipeRepository.count());
-//        recipeBoxRepository.findAll();
-//        System.out.println(recipeBoxRepository.count());
-//
-//        // DELETE
-////        recipeRepository.deleteById(1L);
-////        recipeBoxRepository.deleteById(1L);
-////        recipeRepository.deleteAll();
-////        recipeBoxRepository.deleteAll();
-//    }
-
     @Test
     void contextLoads() {
+        Teacher teacher = new Teacher();
+//        teacher.setId(1L);
+        teacher.setName("김일타");
+        teacherRepository.save(teacher);
+
+        Teacher teacher2 = new Teacher();
+//        teacher2.setId(2L);
+        teacher2.setName("박이타");
+        teacherRepository.save(teacher2);
+
+        Member member = new Member();
+//        member.setId(1L);
+        member.setName("홍길동");
+        member.addTeacher(teacher);
+        member.addTeacher(teacher2);
+        memberRepository.save(member);
+
+//        List<Teacher> teachers = member.getTeachers();// LAZY
+//        teachers.get(0).getName();
+//
+        System.out.println(memberService.findAll());
+        System.out.println(teacherService.findAll());
+//        memberService.findAllMembersNative();
+
+    }
+
+    @Disabled
+    @Test
+    void recipeBoxContextLoads() {
+        UserEntity user = new UserEntity();
+        RecipeBoxEntity recipeBox = new RecipeBoxEntity();
+        RecipeEntity recipe = new RecipeEntity();
+
+        // CREATE
+        // UPDATE
+//        id 에 해당하는 name 변경은 어떻게 하지?
+//        recipeBox.setId(1L);
+        recipeBox.setName("호박레시피");
+        recipeBox.setIsDefault(false);
+        recipeBoxRepository.save(recipeBox);
+
+        recipe.setPeriod("중기");
+        recipe.setOpen("Y");
+        recipe.setQuantity("1회분");
+        recipeBox.add(recipe);
+        recipeRepository.save(recipe);
+
+        // GET
+        recipeRepository.findAll();
+        System.out.println(recipeRepository.count());
+        recipeBoxRepository.findAll();
+        System.out.println(recipeBoxRepository.count());
+
+        // DELETE
+//        recipeRepository.deleteById(1L);
+//        recipeBoxRepository.deleteById(1L);
+//        recipeRepository.deleteAll();
+//        recipeBoxRepository.deleteAll();
+    }
+
+    @Disabled
+    @Test
+    void RecipeBox2ContextLoads() {
         List<RecipeBoxEntity> recipeBoxs = new ArrayList<>();
 //        recipeBoxService.joinRecipe();
         recipeBoxs = recipeBoxService.getAll();
