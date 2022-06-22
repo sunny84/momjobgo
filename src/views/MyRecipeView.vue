31d82bfda64d25a4216ad07f596e23f4cd69e309
Sun <sonya8412@gmail.com>
2022-06-22 12:20:59 +0900

    Add initial MyRecipeView.vue

---
+++ src/views/MyRecipeView.vue
@@ -0,0 +1,28 @@
+<template>
+    <!--HEADER-->
+    <!--CONTENT-->
+        <!--{{$t("content.myRecipe")}} {{ 나의 레시피 수 }}-->
+        <button>{{$t("button.write")}}</button>
+        <!--
+            레시피 목록 기본 노출 3개
+            <a herf="레시피 상세페이지로 이동">{{img}}{{title}}{{sub_title}}{{time}}{{start}}{{?}}</a>
+            <button>레시피 박스 저장</button>
+        -->
+    <!--FOOTER-->
+</template>
+
+<script>
+
+export default {
+    name : "MyRecipeView",
+    data: ()=>({
+
+    }),
+
+    methods : {
+
+    },
+}
+</script>
+<style>
+</style>
