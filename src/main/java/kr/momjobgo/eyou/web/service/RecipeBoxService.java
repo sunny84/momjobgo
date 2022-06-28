0a6eb439364c4e4a81172f4a1405218920da45c1
kusa104 <kusa104@gmail.com>
2022-06-28 12:38:17 +0900

    리스토어해서 올립니다.

--- src/main/java/kr/momjobgo/eyou/web/service/RecipeBoxService.java
+++ src/main/java/kr/momjobgo/eyou/web/service/RecipeBoxService.java
@@ -1,9 +1,24 @@
 package kr.momjobgo.eyou.web.service;
 
 import kr.momjobgo.eyou.web.jpa.entity.RecipeBoxEntity;
+import kr.momjobgo.eyou.web.jpa.entity.TestEntity;
 
 import java.util.List;
 
 public interface RecipeBoxService {
     List<RecipeBoxEntity> joinRecipe();
+
+    List<RecipeBoxEntity> getAll();
+    RecipeBoxEntity getById(Long id);
+    RecipeBoxEntity insertRecipeBoxName(String name);
+    RecipeBoxEntity insertRecipeBox(RecipeBoxEntity entity);
+    RecipeBoxEntity updateRecipeBoxById(Long id, RecipeBoxEntity entity);
+    RecipeBoxEntity updateRecipeBoxNameById(Long id, String name);
+    RecipeBoxEntity updateRecipeBox(RecipeBoxEntity entity);
+    String deleteRecipeBoxById(Long id);
+    String deleteRecipeBoxAll();
+    List<RecipeBoxEntity> findByName(String name);
+    List<RecipeBoxEntity> findByNameContains(String name);
+    RecipeBoxEntity findByIsDefault(Boolean isDefault);
 }
+
