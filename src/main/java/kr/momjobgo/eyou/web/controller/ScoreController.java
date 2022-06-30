d5c47821a0a4ac24016f551488c40c908c38b27c
Sun <sonya8412@gmail.com>
2022-06-30 11:31:56 +0900

    Add Score API
    Add Score API

---
+++ src/main/java/kr/momjobgo/eyou/web/controller/ScoreController.java
@@ -0,0 +1,25 @@
+package kr.momjobgo.eyou.web.controller;
+
+import kr.momjobgo.eyou.web.service.ScoreService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/score")
+public class ScoreController {
+    private final ScoreService scoreService;
+
+    public ScoreController(ScoreService scoreService) { this.scoreService = scoreService; }
+
+    @GetMapping("")
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok().body( scoreService.getAll() );
+    }
+
+    @GetMapping("/id={id}")
+    public ResponseEntity<?> getById(@PathVariable Long id) { return ResponseEntity.ok().body( scoreService.getById(id) ); }
+
+}
