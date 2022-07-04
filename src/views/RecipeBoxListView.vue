c45b9f5edd2bfb6b5e8773f9f753ef87a330e8e9
Sun <sonya8412@gmail.com>
2022-07-04 22:13:16 +0900

    Add RecipeBoxListView.vue
    Add RecipeBoxListView.vue

---
+++ src/views/RecipeBoxListView.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <!--HEADER-->
+    <h4>{{ $t("title.recipeBox") }}</h4>
+    <div class="menu">
+        <ul>
+            <li><a href="/myrecipe">{{ $t("menu.myRecipe") }}</a></li>
+            <li><a href="/recipebox">{{ $t("menu.savedRecipe") }}</a></li>
+            <li><a href="#">{{ $t("menu.historyRecipe") }}</a></li>
+        </ul>
+    </div>
+    <!--CONTENT-->
+    <div class="contents">        
+        <button @click="callEdit">{{$t("button.edit")}}</button>
+        <tr v-for="(item, index) in recipeBoxes" :key="index">
+          <td v-if="item.isDefault == true">
+            <a :href="item.name" target="_blank"> <!--TODO: a herf="레시피 박스 상세 페이지로 이동"-->
+              <p>
+                <img :src="mainPicture" width="200px" height="150px" @error="setEmptyImg">
+              </p>
+              {{item.name}} {{item.recipeEntities.length}}<br/>
+            </a>
+          </td>
+        </tr>
+        <tr v-for="(item, index) in recipeBoxes" :key="index">
+          <td v-if="item.isDefault == false">
+            <a :href="item.name" target="_blank"> <!--TODO: a herf="레시피 박스 상세 페이지로 이동"-->
+              <p>
+                <img :src="mainPicture" width="200px" height="150px" @error="setEmptyImg">
+              </p>
+              {{item.name}} {{item.recipeEntities.length}}<br/>
+            </a>
+          </td>
+        </tr>
+        <tr>          
+          <td>
+            <a href="#" target="_blank"> <!-- TODO: add new box -->
+              <p>
+                <img :src="mainPicture" width="200px" height="150px" @error="setEmptyImg">
+              </p>
+              <br/>
+            </a>
+          </td>
+        </tr>
+    </div>
+    <!--FOOTER-->
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+import emptyImg from '@/assets/emptyImg.png'
+
+export default {
+  name : "RecipeBoxListView",
+  data: ()=>({
+    recipeBoxes : [],
+    mainPicture : ''
+  }),
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      this.callRecipeBox();
+    },
+
+    async callRecipeBox() {
+      await axios.get(`http://localhost:8090/recipebox/all`, {
+      }).then(response=>{
+        this.recipeBoxes = response.data;
+        console.log("contents:", response.data);
+      }).catch(error=>{
+        console.error(error);
+      })
+    },
+    callEdit() {
+      console.log("Edit");
+    },
+    addNewBox() {
+      console.log("addNewBox");
+    },
+    setEmptyImg(e) {
+      e.target.src=emptyImg;
+    },
+  },
+}
+</script>
+
+<style>
+</style>
