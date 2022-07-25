bedcf4036245691231fee880ebafe0b395ecaec6
kangshy <kangshy@gmail.com>
2022-07-25 22:11:02 +0900

    로그인 확인 및 로그인 후 페이지 이동
    - 로그인여부를 확인하기 위한 공통함수 $checkToken 을 mixins에 추가
    - 로그인 후 원하는 페이지로 이동하도록 loginView 수정
    - 원하는 페이지가 없으면 홈으로 이동

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,12 +1,17 @@
 import axios from "@/plugins/axios";
 import store from "@/store";
 import Code from "@/static/Code";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   data: () => ({
     ...Code,
   }),
   
+  computed : {
+    ...mapGetters("user", ["hasToken"]),
+  },
+
   methods: {
     /**
      * 공통 API 호출 함수
@@ -46,6 +51,13 @@ export default {
         }
         return e.response;
       });
+    },
+    $checkToken(nextUrl) {
+      // 현재 토근이 있는지 확인
+      if(!this.hasToken) {
+        // 토큰이 없을 때 로그인 페이지로 이동
+        this.$router.push({path : "/login/"+nextUrl });
+      }
     },
   },
 };
