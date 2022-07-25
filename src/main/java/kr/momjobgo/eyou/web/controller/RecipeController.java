8c2bf8a97a4a9be53022dd2d17930bb1cd988302
kusa104 <kusa104@gmail.com>
2022-07-25 21:07:26 +0900

    필터 옮김

--- src/main/java/kr/momjobgo/eyou/web/controller/RecipeController.java
+++ src/main/java/kr/momjobgo/eyou/web/controller/RecipeController.java
@@ -17,25 +17,6 @@ public class RecipeController {
         this.recipeService = recipeService;
      }
 
-    @GetMapping("/api/filter")
-    public ResponseEntity<?> getFilterList(
-            @RequestParam(value = "page") int page,
-            @RequestParam(value = "sort", required = false) Long sort,
-            @RequestParam(value = "period", required = false) Long period,
-            @RequestParam(value = "timeTakenId", required = false) Long timeTakenId,
-            @RequestParam(value = "Ids", required = false) String ids) {
-                List<Long> Ids = new ArrayList<>();
-                if (ids != null) {
-                    String[] arr = ids.split(",");
-                    for (int i = 0; i < arr.length; i++) {
-                        Ids.add(Long.parseLong(arr[i]));
-                    }
-                }
-                Long userId = UserManager.getUser().getId();
-                System.out.println("===> user ID : "+ userId);
-                return ResponseEntity.ok().body( recipeService.findByFilter(PageRequest.of(page, 2), userId, sort, period, timeTakenId, Ids) );
-            }
-
     @GetMapping("")
     public ResponseEntity<?> getAll() {
         return ResponseEntity.ok().body( recipeService.getAll() );
