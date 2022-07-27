a236c86e739f68877c70c1192f6f1023eb752435
kusa104 <kusa104@gmail.com>
2022-07-27 23:19:52 +0900

    인피니트로딩 적용 안되어 수정

--- src/views/RecipeListView.vue
+++ src/views/RecipeListView.vue
@@ -2,13 +2,13 @@
   <div>
     <main>
       <div class="wrap">
-        <h1><span class="color-orange">우리아기,</span> 
-          <br>어떤 요리가 좋을까요?</h1>
+        <h1><span class="color-orange">{{$t(`title.myBaby`)}},</span> 
+          <br>{{$t(`title.whatRecipe`)}}</h1>
         <SubscribeListView></SubscribeListView>
         <div class="wrap_filter">
           <div class="wrap_filter_menu">
             <ul class="fl">
-              <li class="title"><router-link to="/filter">필터</router-link></li>
+              <li class="title"><router-link to="/filter">{{$t(`title.RecipeFilter`)}}</router-link></li>
               <li class="menu" :class="{ on : sort == 0 }" @click="setSort(0)">최신순</li>
               <li class="menu" :class="{ on : sort == 1 }" @click="setSort(1)">별점순</li>
               <li class="menu">후기순</li>
