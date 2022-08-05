37e7ea9c3766a73d3a89f790a05fb0a3136d6ead
Sun <sonya8412@gmail.com>
2022-08-05 10:14:06 +0900

    vue2 basic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div>
+  <Fruits />
+  <!-- <h1>{{ reverseMessage() }}</h1> -->
+  <!-- <button @click="add">Add</button> -->
+  <!-- <h1 
+    :class="{ active: isActive }"
+    @click="activate">
+    {{ msg }}{{ isActive }}
+  </h1> -->
+  <MyBtn>Apple</MyBtn>
+  <MyBtn large color="green"><span style="color:yellow">Banana</span></MyBtn>
+  
+  <!-- <h1>{{ reversedMesage }}</h1> -->
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Fruits from './components/Fruits.vue'
+import MyBtn from './components/MyBtn.vue'
 export default {
-  name: 'App',
+  data() {
+    return {
+      // Getter, Setter
+      msg: 'Hello Computed!',
+      isActive: false
+    }
+  },
+  computed: {
+    // // Getter
+    // resersedMesage() {  // 캐싱된 데이터값
+    //   return this.msg.split('').reverse().join('')
+    // }
+    reversedMesage: {
+      get() {
+        return this.msg.split('').reverse().join('')
+      },
+      set(value) {
+        console.log(value)
+        this.msg = value
+      }
+    }
+  },
+  watch: { // 데이터 감시 후 로직의 처리 가능
+    msg(newValue) {
+      console.log('msg:', newValue)
+    },
+    reversedMesage(newValue) {
+      console.log('reversedMessage:', newValue)
+    }
+  },
+  methods: {
+    reverseMessage() {
+      return this.msg.split('').reverse().join('')
+    },
+    add() {
+      this.reversedMesage += '!?'
+      // this.msg += '!?'
+    },
+    changeMessage() {
+      this.msg = 'Good!'
+    },
+    activate() {
+      this.isActive = !this.isActive
+    },
+  },
   components: {
-    HelloWorld
-  }
+    Fruits,
+    MyBtn
+}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .active {
+    color: red;
+    font-weight: bold;
+  }
 </style>
---
+++ src/components/Fruits.vue
@@ -0,0 +1,54 @@
+<template>
+<!-- 템플릿 문법/Computed/Getter, Setter/Watch -->
+  <section>
+    <div>
+      <h1>Fruits</h1>
+      <ul>
+        <li
+          v-for="fruit in fruits"
+          :key="fruit">
+          {{ fruit }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h1>Reverse Fruits</h1>
+      <ul>
+        <li
+          v-for="fruit in reverseFruits"
+          :key="fruit">
+          {{ fruit }}
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+<script>
+export default{
+  data() {
+    return {
+      fruits: [
+        'Apple', 'Banana', 'Cherry'
+      ]
+    }    
+  },
+  computed: {
+    hasFruit() {
+      return this.fruits.length > 0
+    },
+    reverseFruits() {
+      return this.fruits.map(fruit => {
+        // 'Apple' => ['A','p','p','l','e']
+        // => ['e', 'l', 'p', 'p', 'A'] => 'elppA'
+        return fruit.split('').reverse().join('')
+      })
+    }
+  }
+}
+</script>
+<style scoped>
+ul{
+  list-style:none;
+  padding-left:0px;
+}
+</style>
