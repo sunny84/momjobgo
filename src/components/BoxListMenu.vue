38f43c2bd4a2622a70dc9153214e117a8d223f08
Sun <sonya8412@gmail.com>
2022-08-21 13:50:51 +0900

    Fix/Chg 히스토리 스택에서 뒤 페이지로 이동
    Fix/Chg 히스토리 스택에서 뒤 페이지로 이동

--- src/components/BoxListMenu.vue
+++ src/components/BoxListMenu.vue
@@ -1,6 +1,6 @@
 <template>
     <div>   
-        <h1 class="fl"><router-link :to="'/recipedetail/'+recipeId"><img src="@/assets/images/icon_back.png" alt="돌아가기" title="돌아가기"/></router-link>
+        <h1 class="fl"><div class="button btn2 btn-link" @click="$router.go(-1)"><img src="@/assets/images/icon_back.png" alt="돌아가기" title="돌아가기"/></div>
         <span class="color-orange padding-left-15">{{ $t("title.recipeBox") }}</span>
         </h1>
         <div class="wrap_menu">
